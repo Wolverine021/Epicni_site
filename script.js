0dcbdf5d3683b232accedee05ce601e0372731d8
Duje Marasovic <duje.maras@gmail.com>
2024-10-04 00:36:11 +0200

    drugi komit

---
+++ script.js
@@ -0,0 +1,7 @@
+function funk(event) {
+    event.preventDefault();
+    document.getElementById("poruka").innerText = "Hvala!";
+  }
+function myFunction() {
+    document.getElementById("appolo").innerHTML = "Decu !";
+}
